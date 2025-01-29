90f7030b690811f9f0268849a1f658334f342789
Keegan S. <62641690+ksalisbury-gh@users.noreply.github.com>
2025-01-29 12:16:05 -0600

    yes

---
+++ knock.py
@@ -0,0 +1,9 @@
+LIKES_FD = input("Do you like fish sticks?: ")
+if LIKES_FD[0] == 'Y' or 'y':
+    LIKES_FD_IN_MOUTH = input("Do you like putting fish sticks in your mouth?: ")
+    if LIKES_FD_IN_MOUTH[0] == 'Y' or 'y':
+        print("What are you, a GAY FISH?")
+    else:
+        print("You have defeated me! :(")
+else:
+    print("You have defeated me! :(")
